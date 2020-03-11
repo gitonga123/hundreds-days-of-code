0c9906abcd2e8797ce052a4950e6434828f68578
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2020-03-11 15:51:18 +0300

    Django Rest - Create get endpoints

--- movieapi/api/views.py
+++ movieapi/api/views.py
@@ -1,3 +1,30 @@
+from .serializers import movieSerializer, actorSerializer
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import api_view
+from . models import movie, actor
+from . serializers import movieSerializer, actorSerializer
 
 # Create your views here.
+class JSONResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+def index(request):
+    return HttpResponse("&lt;h3&gt;Welcome to Movie API v1.0&lt;/h3&gt;")
+
+@api_view(['GET'])
+def movies(request):
+    movies = movie.objects.all()
+    serializer = movieSerializer(movies, many=True)
+    return JSONResponse(serializer.data)
+
+
+@api_view(['GET'])
+def actors(request):
+    actors = actor.objects.all()
+    serializer = actorSerializer(actors, many=True)
+    return JSONResponse(serializer.data)

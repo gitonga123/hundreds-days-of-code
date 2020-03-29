863e391f7e0f742935b2306d4c331080e83f712e
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2020-03-29 19:11:44 +0300

    Widget: ListView - Basic List View

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -9,7 +9,46 @@ class MyHome extends StatelessWidget {
     return MaterialApp(
       title: "Exploring Flutter UI Widgets",
       debugShowCheckedModeBanner: false,
-      home: Home()
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Basic List View"
+          ),
+        ),
+        body: getListView(),
+      )
     );
   }
+}
+
+Widget getListView() {
+  var listView = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful view !"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () {
+          debugPrint("Landscape Tapped");
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_chromebook),
+        title: Text("Windows"),
+        subtitle: Text("My Computers"),
+        trailing: Icon(Icons.computer),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Phone"),
+      ),
+      Text("Yet another element in List"),
+      Container(
+        color: Colors.red,
+        height: 50.0,
+      )
+    ],
+  );
+  return listView;
 }

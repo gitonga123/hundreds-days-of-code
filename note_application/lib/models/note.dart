8d541d956d99e6e6ae14c46dfa5b350491e81dc8
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2020-04-05 18:21:26 +0300

    Notekeeper Database: Create a model class

---
+++ note_application/lib/models/note.dart
@@ -0,0 +1,58 @@
+class Note {
+  int _id;
+  String _title;
+  String _description;
+  String _date;
+  int _priority;
+
+  Note(this._title, this._date, this._priority, [this._description]);
+
+  Note.widthId(this._id, this._title, this._date, this._priority, [this._description]);
+
+  int get id => _id;
+  String get title => _title;
+  String get description => _description;
+  String get date => _date;
+  int get priority => _priority;
+
+  set title(String newTitle) {
+    if (newTitle.isNotEmpty && newTitle.length <= 255) {
+      this._title = newTitle;
+    }
+  }
+  set description(String newDescription) {
+    if (newDescription.isNotEmpty && newDescription.length <= 255) {
+      this._description = newDescription;
+    }
+  }
+  set priority(int newPriority) {
+    if (newPriority >= 1 && newPriority <= 1) {
+      this._priority = newPriority;
+    }
+  }
+
+  set date(String newDate) {
+    this._date = newDate;
+  }
+
+  // Convert a note Object into a map object
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if(id != null) {
+      map['id'] = _id;
+    }
+    map['title'] = _title;
+    map['description'] = _description;
+    map['date'] = _date;
+
+    return map;
+  }
+
+  Note.fromMap(Map<String, dynamic> map) {
+    this._id = map['id'];
+    this._title = map['title'];
+    this._description = map['description'];
+    this._date = map['date'];
+    this._priority = map['priority'];
+  }
+}

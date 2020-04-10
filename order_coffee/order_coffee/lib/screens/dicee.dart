8a422f48b29a64be7f26832920e6285767a501bd
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2020-04-10 08:36:19 +0300

    Roll up the dice: Expanded, Math class, Set State

--- order_coffee/order_coffee/lib/screens/dicee.dart
+++ order_coffee/order_coffee/lib/screens/dicee.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,12 +18,34 @@ class DicePage extends StatelessWidget {
         title: Text('Dicee'),
         backgroundColor: Colors.red,
       ),
-      body: Row(
-        children: <Widget>[
-          Expanded(child: Image.asset('assets/images/dice1.png')),
-          Expanded(child: Image.asset('assets/images/dice1.png'))
-        ],
+      body: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                child: Image.asset('assets/images/dice$leftDiceNumber.png'),
+                onPressed: () {
+                    updateDiceNumber();
+                },
+              ),
+            ),
+            Expanded(
+                child: FlatButton(
+                  child: Image.asset('assets/images/dice$rightDiceNumber.png'),
+                  onPressed: () {
+                      updateDiceNumber();
+                  },
+                )),
+          ],
+        ),
       ),
     );
+  }
+
+  void updateDiceNumber() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+      leftDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }

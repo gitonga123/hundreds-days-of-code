90a194a4e6b7b076835819b1d9a0b648df69ad71
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2020-04-18 16:46:44 +0300

    Quiz App - Question Model, Counter Correct Questions, QuizBrain

---
+++ order_coffee/order_coffee/lib/classes_objects.dart
@@ -0,0 +1,15 @@
+import 'main.dart';
+
+class Car {
+  int numberOfDoors = 5;
+
+  void drive() {
+    print('Wheels start tuning');
+  }
+}
+
+void main() {
+  Car premio = Car();
+
+  premio.drive();
+}

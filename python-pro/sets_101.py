e8914642a7f3c166d87722dd557243f866cf6258
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2020-04-28 15:52:03 +0300

    Getting the Number of Elements in a Set

--- python-pro/sets_101.py
+++ python-pro/sets_101.py
@@ -3,3 +3,11 @@ myset = set()
 myset_2 = set([1, 3, 5, 6])
 myset_3 = set("abc")
 print(myset_2)
+
+# Getting the Number of Elements in a Set
+print(len(myset_2))
+
+# 
+myset.add(1)
+myset.add(3)
+print(myset)

3161e4cc515fe0b9593401cf3cb600b0fd17a9bb
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2020-04-28 16:00:56 +0300

    Union of two sets

--- python-pro/sets_101.py
+++ python-pro/sets_101.py
@@ -29,4 +29,13 @@ if "c" in myset:
 
 if "d" not in myset:
     print("Am not in the set")
-    
+
+# Union of two sets
+new_set = myset | myset_2 | myset_3
+print(new_set)
+
+set1 = set([1, 2, 3, 4])
+set2 = set([3, 4, 5, 6])
+
+set3 = set1.union(set2)
+

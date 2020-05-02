df16cc634f6d9aed039deeab3f9f0a4ea21f4fe9
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2020-05-02 21:10:40 +0300

    Flutter Widget of the week: AnimatedWidget

--- order_coffee/order_coffee/lib/main.dart
+++ order_coffee/order_coffee/lib/main.dart
@@ -14,13 +14,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Login UI',
+      title: 'Animated Widget - Widget of the week',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       // home: MyHomePage()
-      home: ClipOvalWidget()
+      home: AnimatedWidgetWow()
     );
   }
 }

55e22b9cb2e91c10fd3c780b926b7aef13492332
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2020-05-03 17:37:59 +0300

    Recursion - GCD

---
+++ python-pro/gcd.py
@@ -0,0 +1,17 @@
+# This program uses recursion to find the GCD
+# of two numbers
+
+def main():
+    num1 = int(input('Enter an integer: '))
+    num2 = int(input('Enter another integer: '))
+
+    print('The greatest common divisor of')
+    print('the two number is', gcd(num1, num2))
+
+def gcd(x, y):
+    if x % y == 0:
+        return y
+    else:
+        return gcd(x, x % y)
+
+main()
--- python-pro/pytest/py_math.py
+++ python-pro/pytest/py_math.py
@@ -1,5 +1,7 @@
+import pytest
+
 def add(x, y):
     return x + y
 
 def test_add():
-    assert add(1, 2) == 4
+    assert add(1, 2) == 2

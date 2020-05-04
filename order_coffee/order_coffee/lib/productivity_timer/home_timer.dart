3c53f8864790eea6ab945cc95f1d406cd1c86eb4
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2020-05-04 17:22:44 +0300

    Productivity Timer - Initial Setup

---
+++ order_coffee/order_coffee/lib/productivity_timer/home_timer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProductivityTimer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Work Timer"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("My Work Timer"),
+      ),
+    );
+  }
+}

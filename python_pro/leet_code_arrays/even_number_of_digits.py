8bfc5935c9e5651838a5399f56a3986ff31d1355
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2020-05-08 11:23:11 +0300

    Find Numbers with Even Number of Digits

--- python_pro/leet_code_arrays/even_number_of_digits.py
+++ python_pro/leet_code_arrays/even_number_of_digits.py
@@ -3,15 +3,13 @@ import math
 
 def findNumbers(n):
     counter = 0
-    numbers_count = [0]
     for i in n:
         digits = int(math.log10(i))+1
         if digits % 2 == 0:
             counter += 1
-            if (counter > numbers_count[0]):
-                numbers_count[0] = counter
+            
     
-    return numbers_count[0]
+    return counter
         
 
 print(findNumbers([555,901,482,1771]))

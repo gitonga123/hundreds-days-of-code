a7c3d3e0eae40a3d6b62a8fdd2fc1fc33b5da443
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2020-05-08 10:16:09 +0300

    Padding and aligning strings

--- python_pro/py_format.py
+++ python_pro/py_format.py
@@ -4,3 +4,28 @@ print('{} {}'.format('one', 'two'))
 print("%d %d" % (1, 2))
 print('{} {}'.format(1, 2))
 print('{1} {0}'.format(1, 2))
+
+# Value conversion
+
+class Data(object):
+    def __str__(self):
+        return 'str'
+    
+    def __repr__(self):
+        return 'repr'
+    
+print("%s %r" %(Data(), Data()))
+print('{0!s} {0!r}'.format(Data()))
+
+# Padding and aligning strings
+
+# align right
+print("%10s" % ('test'))
+print('{:>10}'.format('test'))
+
+# align left
+print("%-10s" % ('test'))
+print('{:10}'.format('test'))
+
+print('{:_<10}'.format('test'))
+print('{:^10}'.format('test'))

97608227ed2f8aef0c3c68455ed2a972c1740335
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2020-05-14 21:41:55 +0300

    LeetCode - Check If N and Its Double Exist

--- python_pro/check_double_exists.py
+++ python_pro/check_double_exists.py
@@ -8,18 +8,19 @@ i != j
 arr[i] == 2 * arr[j]
 
 """
-arr = [10,2,5,3]
+arr = [3,1,7,11]
 
 def checkIfExist(arr):
-    s = set()
+    if len(arr) == 0:
+            return False
+    s = []
     for num in arr:
         if num not in s:
             if num % 2 == 0:
-                s.add(num // 2)
-            s.add(num * 2)
+                s.append(num // 2)
+            s.append(num * 2)
         else:
             return True
-    
     return False
 
 print(checkIfExist(arr))

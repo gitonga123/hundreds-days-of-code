2e64462453ca11f3dbed51fbb9bb6af1afff2d74
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2020-05-16 16:01:14 +0300

    Using stacks: Conver decimal to binary to base 2 and other bases too

---
+++ python_pro/convert_binary.py
@@ -0,0 +1,37 @@
+from stacks import Stack
+
+
+def divideBy2(decNumber):
+    remStack = Stack()
+
+    while decNumber > 0:
+        rem = decNumber % 2
+        remStack.push(rem)
+        decNumber = decNumber // 2
+
+    binString = ""
+
+    while not remStack.isEmpty():
+        binString = binString + str(remStack.pop())
+
+    return binString
+
+def baseConverter(decNumber, base):
+    digits = "0123456789ABCDEF"
+    remStack = Stack()
+
+    while decNumber > 0:
+        rem = decNumber % base
+        remStack.push(rem)
+        decNumber = decNumber // base
+    
+    new_string = ""
+
+    while not remStack.isEmpty():
+        new_string = new_string + digits[remStack.pop()]
+    
+    return new_string
+
+print(divideBy2(42))
+print(baseConverter(25, 2))
+print(baseConverter(25, 16))

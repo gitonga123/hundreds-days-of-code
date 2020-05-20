4e02ed2f99c9712d3cbad08c9f01133cbb647468
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2020-05-20 14:38:55 +0300

    Play Movie Trailer

---
+++ order_coffee/order_coffee/lib/shopping/util/dbhelper.dart
@@ -0,0 +1,11 @@
+import 'package:sqflite/sqlite_api.dart';
+import 'package:path/path.dart';
+
+class DbHelper {
+  final int version = 1;
+  Database db;
+
+  Future<Database> openDb() async {
+
+  }
+}

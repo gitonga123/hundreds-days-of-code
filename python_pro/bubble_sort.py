308cebaded28b4864101d82d4c31e825c516ec52
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2020-05-30 11:54:56 +0300

    Bubble Sort - python

--- python_pro/bubble_sort.py
+++ python_pro/bubble_sort.py
@@ -1,10 +1,10 @@
-def bubbleSort(arr):
-    for num in range(len(arr)-1, 0, -1):
-        for i in range(num):
-            if arr[i] > arr[i + 1]:
-                temp = arr[i]
-                arr[i] = arr[i+1]
-                arr[i+1] = temp
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1, 0, -1):
+        for i in range(passnum):
+            if alist[i] > alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
 
 
 alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]

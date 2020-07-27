b14d3ed8c9f0ef052483cb5024d9bce9253976e7
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-07-27 10:42:08 +0300

    __repr__

--- python_pro/linked_list/node_class.py
+++ python_pro/linked_list/node_class.py
@@ -5,11 +5,23 @@ class Node:
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
+    
+    def __repr__(self):
+        return self.data
 
 # A Linked List class with a single head node
 class LinkedList:
     def __init__(self):
         self.head = None
+    
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
 
     # insertion method for the linked list
     def insert(self, data):

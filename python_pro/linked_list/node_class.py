7df95a50bfdb16ed3cdfda084d7087e7f27edff2
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-07-27 10:00:46 +0300

    Node class LinkedList Class with head

---
+++ python_pro/linked_list/node_class.py
@@ -0,0 +1,20 @@
+# A single note of a singly linked list
+
+class Node:
+    # Constructor
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+# A Linked List class with a single head node
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+
+# Linked List with a single node
+LL = LinkedList()
+LL.head = Node(3)
+print(LL.head.data)
+

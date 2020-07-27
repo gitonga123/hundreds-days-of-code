e416ef6a91a563a043ebcdc25a62e5c935907ed4
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-07-27 10:04:42 +0300

    Singly Linked List with insertion and print methods

--- python_pro/linked_list/node_class.py
+++ python_pro/linked_list/node_class.py
@@ -11,6 +11,24 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    # insertion method for the linked list
+    def insert(self, data):
+        newNode = Node(data)
+        if self.head:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = newNode
+        else:
+            self.head = newNode
+    
+    # print method for the linked list
+    def printLL(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
 
 
 # Linked List with a single node
